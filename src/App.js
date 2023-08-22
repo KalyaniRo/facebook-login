8e3e16f455b309d658b0c64c8021c0aec0e0205a
KalyaniRo <kalyaniavaghade1307@gmail.com>
2023-08-22 14:30:25 +0530

    Initialize project uhttps://github.com/KalyaniRo/facebook-login.gitsing Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div class="container">
+
+        <h1>Facebook</h1>
+        <p>Facebook help you connect and share <br /> with the pepople in your life </p>
+
+        <div className='box'>
+          <input type='email' name='email' id='email' placeholder='Email or Phone number'></input><br />
+          <input type='password' name='password' id='password' placeholder='Password'></input>
+
+          <div class="link">
+
+            <a href='#'>Log In</a>
+
+          </div>
+
+          <div class="link1">
+            <a href='#'>Forgotten password</a>
+
+          </div>
+          <div class="link2">
+            <a href='#'>Create New Account</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
